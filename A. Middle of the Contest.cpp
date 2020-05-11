9dd58f5e1d54008795a5ca03fcc7d14cec16f740
Anisur Rahaman <60539253+Anisur-Rahaman00@users.noreply.github.com>
2020-05-11 22:23:50 +0600

    Add files via upload

---
+++ A. Middle of the Contest.cpp
@@ -0,0 +1,124 @@
+/*
+
+ ALLAH IS ALMIGHTY              ******BISMILLAHIR RAHMANIR RAHIM.******              ALLAH IS ALMIGHTY
+                                    ```````````````````````````````
+                                        AUTHOR: ANISUR RAHAMAN
+                                            BSMRSTU(SHIICT)
+                                                ID:017
+
+                                               !!!!!!!!
+                                               !!!!!!!!
+                                               !!!!!!!!
+                                               !!!!!!!!
+                                               !!!!!!!!
+                                               !!!!!!!!
+                                               !!!!!!!!
+                                               !!!!!!!!
+                                               !!!!!!!!
+                                           ****!!!!!!!!****
+                                            **************
+                                             ************
+                                              **********
+                                               ********
+                                                ******
+                                                 ****
+                                                  **
+
+ _____________________________________________________________________________________________________
+ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+ `````````````````````````````````````````````````````````````````````````````````````````````````````
+ ******                                                                                         ******
+ *****                                                                                           *****
+ ****                                                                                             ****
+ ***                                                                                               ***
+ **                                                                                                 **
+ *                                                                                                   *
+         $$$$$$$$$$$$$$         $$$$$$           $$$$$$$$    $$$$$$$$$$        $$$$$$$$$$$$$$$$
+       $$$$$$$$$$$$$$$$$$       $$$$$$$           $$$$$$    $$$$$$$$$$$$      $$$$$$$$$$$$$$$$$$$
+      $$$$$$$$$$$$$$$$$$$$      $$$$ $$$           $$$$    $$$$$$$$$$$$$$    $$$$$$$$$$$$$$$$$$$$$
+      $$$$            $$$$      $$$$  $$$          $$$$         $$$$         $$$$$
+      $$$$            $$$$      $$$$   $$$         $$$$         $$$$         $$$$$
+      $$$$            $$$$      $$$$    $$$        $$$$         $$$$         $$$$$
+      $$$$     $$     $$$$      $$$$     $$$       $$$$         $$$$         $$$$$$$$$$$$$$$$$$
+      $$$$$$$$$$$$$$$$$$$$      $$$$      $$$      $$$$         $$$$          $$$$$$$$$$$$$$$$$$
+      $$$$$$$$$$$$$$$$$$$$      $$$$       $$$     $$$$         $$$$           $$$$$$$$$$$$$$$$$$
+      $$$$            $$$$      $$$$        $$$    $$$$         $$$$                        $$$$$
+      $$$$            $$$$      $$$$         $$$   $$$$         $$$$                        $$$$$
+      $$$$            $$$$      $$$$          $$$  $$$$         $$$$                        $$$$$
+      $$$$            $$$$      $$$$           $$$ $$$$    $$$$$$$$$$$$$$   $$$$$$$$$$$$$$$$$$$$$
+     $$$$$$          $$$$$$    $$$$$$           $$$$$$$     $$$$$$$$$$$$     $$$$$$$$$$$$$$$$$$$
+    $$$$$$$$        $$$$$$$$  $$$$$$$$           $$$$$$      $$$$$$$$$$        $$$$$$$$$$$$$$$$
+
+                            LETS START OUR JOURNEY...................
+
+*/
+
+#include<bits/stdc++.h>
+#define speed ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define pb push_back
+#define pf push_front
+#define in insert
+#define ff first
+#define ss second
+#define off return 0
+#define mp make_pair
+#define MOD 1000000007
+#define ll long long int
+#define ld long double
+#define snll(a) scanf("%lld", &a)
+#define snll2(a,b) scanf("%lld %lld", &a,&b)
+#define snll3(a,b,c) scanf("%lld %lld %lld", &a,&b,&c)
+#define snl(b) scanf("%d", &b)
+#define snl2(a,b) scanf("%d %d", &a,&b)
+#define snl3(a,b,c) scanf("%d %d %d", &a,&b,&c)
+#define ptl(a) printf("%d\n", a)
+#define ptll(a) printf("%lld\n", a)
+#define ptl2(a,b) printf("%d %d\n", a,b)
+#define ptll2(a,b) printf("%lld %lld\n", a,b)
+#define gcd(a,b) __gcd(a,b)
+#define lcm(a,b) (a*b)/gcd(a,b)
+#define pi acos(-1)
+#define C continue
+#define B break
+#define N '\n'
+#define al(v) v.begin(),v.end()
+#define srt(v) sort(v.begin(),v.end())
+#define rev(v) reverse(v.begin(),v.end())
+#define loop(i,a,n) for(int i=a;i<n;i++)
+#define T int ta; scanf("%d", &ta); while(ta--)
+using namespace std;
+int main()
+{
+    speed
+    int a,b,c,d;
+    char ch1,ch2;
+    cin>>a>>ch1>>b;
+    cin>>c>>ch2>>d;
+    if(a<=c)
+    {
+        int ha=a*60+b,hb=c*60+d;
+        int hour=(hb-ha);
+        int hh=ha+hour/2;
+        int ans=hh/60;
+        int minute=hh%60;
+        cout << setw(2) << setfill('0') << ans << ":";
+        cout << setw(2) << setfill('0') << minute << N;
+    }
+    else
+    {
+        int ha=a*60+b;
+        int hb=(1440-ha)+c*60+d;
+        cout << hb << N;
+        int hh=ha+hb/2;
+        int ans=hh/60;
+        int minute=hh%60;
+        if(ans>24)
+            ans=ans%24;
+        cout << setw(2) << setfill('0') << ans << ":";
+        cout << setw(2) << setfill('0') << minute << N;
+
+    }
+    off;
+}
+
