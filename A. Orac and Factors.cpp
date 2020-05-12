f44655106fff3004677b8e5474393b1f9049c7c2
Anisur Rahaman <60539253+Anisur-Rahaman00@users.noreply.github.com>
2020-05-12 23:35:00 +0600

    Add files via upload

---
+++ A. Orac and Factors.cpp
@@ -0,0 +1,131 @@
+/*
+
+ ALLAH IS ALMIGHTY              ******BISMILLAHIR RAHMANIR RAHIM.******              ALLAH IS ALMIGHTY
+                                    ```````````````````````````````
+                                        AUTHOR: ANISUR RAHAMAN
+                                            BSMRSTU(SHIICT)
+                                                ID:017
+
+                                               !!!!!!!!
+                                               !!!!!!!!
+                                               !!!!!!!!
+                                               !!!!!!!!
+                                               !!!!!!!!
+                                               !!!!!!!!
+                                               !!!!!!!!
+                                               !!!!!!!!
+                                               !!!!!!!!
+                                           ****!!!!!!!!****
+                                            **************
+                                             ************
+                                              **********
+                                               ********
+                                                ******
+                                                 ****
+                                                  **
+
+ _____________________________________________________________________________________________________
+ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+ `````````````````````````````````````````````````````````````````````````````````````````````````````
+ ******                                                                                         ******
+ *****                                                                                           *****
+ ****                                                                                             ****
+ ***                                                                                               ***
+ **                                                                                                 **
+ *                                                                                                   *
+         $$$$$$$$$$$$$$         $$$$$$           $$$$$$$$    $$$$$$$$$$        $$$$$$$$$$$$$$$$
+       $$$$$$$$$$$$$$$$$$       $$$$$$$           $$$$$$    $$$$$$$$$$$$      $$$$$$$$$$$$$$$$$$$
+      $$$$$$$$$$$$$$$$$$$$      $$$$ $$$           $$$$    $$$$$$$$$$$$$$    $$$$$$$$$$$$$$$$$$$$$
+      $$$$            $$$$      $$$$  $$$          $$$$         $$$$         $$$$$
+      $$$$            $$$$      $$$$   $$$         $$$$         $$$$         $$$$$
+      $$$$            $$$$      $$$$    $$$        $$$$         $$$$         $$$$$
+      $$$$     $$     $$$$      $$$$     $$$       $$$$         $$$$         $$$$$$$$$$$$$$$$$$
+      $$$$$$$$$$$$$$$$$$$$      $$$$      $$$      $$$$         $$$$          $$$$$$$$$$$$$$$$$$
+      $$$$$$$$$$$$$$$$$$$$      $$$$       $$$     $$$$         $$$$           $$$$$$$$$$$$$$$$$$
+      $$$$            $$$$      $$$$        $$$    $$$$         $$$$                        $$$$$
+      $$$$            $$$$      $$$$         $$$   $$$$         $$$$                        $$$$$
+      $$$$            $$$$      $$$$          $$$  $$$$         $$$$                        $$$$$
+      $$$$            $$$$      $$$$           $$$ $$$$    $$$$$$$$$$$$$$   $$$$$$$$$$$$$$$$$$$$$
+     $$$$$$          $$$$$$    $$$$$$           $$$$$$$     $$$$$$$$$$$$     $$$$$$$$$$$$$$$$$$$
+    $$$$$$$$        $$$$$$$$  $$$$$$$$           $$$$$$      $$$$$$$$$$        $$$$$$$$$$$$$$$$
+
+                            LETS START OUR JOURNEY...................
+
+*/
+
+#include<bits/stdc++.h>
+#define speed ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define pb push_back
+#define pf push_front
+#define in insert
+#define ff first
+#define ss second
+#define off return 0
+#define mp make_pair
+#define MOD 1000000007
+#define ll long long int
+#define ld long double
+#define snll(a) scanf("%lld", &a)
+#define snll2(a,b) scanf("%lld %lld", &a,&b)
+#define snll3(a,b,c) scanf("%lld %lld %lld", &a,&b,&c)
+#define snl(b) scanf("%d", &b)
+#define snl2(a,b) scanf("%d %d", &a,&b)
+#define snl3(a,b,c) scanf("%d %d %d", &a,&b,&c)
+#define ptl(a) printf("%d\n", a)
+#define ptll(a) printf("%lld\n", a)
+#define ptl2(a,b) printf("%d %d\n", a,b)
+#define ptll2(a,b) printf("%lld %lld\n", a,b)
+#define gcd(a,b) __gcd(a,b)
+#define lcm(a,b) (a*b)/gcd(a,b)
+#define pi acos(-1)
+#define C continue
+#define B break
+#define N '\n'
+#define al(v) v.begin(),v.end()
+#define srt(v) sort(v.begin(),v.end())
+#define rev(v) reverse(v.begin(),v.end())
+#define loop(i,a,n) for(int i=a;i<n;i++)
+#define T int ta; scanf("%d", &ta); while(ta--)
+using namespace std;
+ll prim(ll n)
+{
+    for(int i=2; i*i<=n; i++)
+    {
+        if(n%i==0)
+            return false;
+    }
+    return true;
+}
+int main()
+{
+    speed
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        ll n,k,ans,mn;
+        cin>>n>>k;
+        if(n%2==0)
+            cout << n+2*k << N;
+        else
+        {
+            if(prim(n))
+                cout << 2*n+((k-1)*2)<< N;
+            else
+            {
+                for(int i=2; i<=n; i++)
+                {
+                    if(n%i==0)
+                    {
+                        mn=i;
+                        B;
+                    }
+                }
+                cout << n+mn+(k-1)*2 << N;
+            }
+        }
+    }
+    off;
+}
+
