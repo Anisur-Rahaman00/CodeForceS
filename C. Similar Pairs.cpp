d1d03b03efbb17478bf5d1f68be9508963c96b2f
Anisur Rahaman <60539253+Anisur-Rahaman00@users.noreply.github.com>
2020-05-25 00:13:27 +0600

    Add files via upload

---
+++ C. Similar Pairs.cpp
@@ -0,0 +1,136 @@
+/*
+
+ ALLAH IS ALMIGHTY              ******BISMILLAHIR RAHMANIR RAHIM.******              ALLAH IS ALMIGHTY
+                                    ```````````````````````````````
+                                        AUTHOR: ANISUR RAHAMAN
+                                            BSMRSTU(SHIICT)
+                                                ID:017
+
+                                               !!!!!!!!
+                                               !!!!!!!!
+                                               !!!!!!!!
+                                               !!!!!!!!
+                                               !!!!!!!!
+                                               !!!!!!!!
+                                               !!!!!!!!
+                                               !!!!!!!!
+                                               !!!!!!!!
+                                           ****!!!!!!!!****
+                                            **************
+                                             ************
+                                              **********
+                                               ********
+                                                ******
+                                                 ****
+                                                  **
+
+ _____________________________________________________________________________________________________
+ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+ `````````````````````````````````````````````````````````````````````````````````````````````````````
+ ******                                                                                         ******
+ *****                                                                                           *****
+ ****                                                                                             ****
+ ***                                                                                               ***
+ **                                                                                                 **
+ *                                                                                                   *
+         $$$$$$$$$$$$$$         $$$$$$           $$$$$$$$    $$$$$$$$$$        $$$$$$$$$$$$$$$$
+       $$$$$$$$$$$$$$$$$$       $$$$$$$           $$$$$$    $$$$$$$$$$$$      $$$$$$$$$$$$$$$$$$$
+      $$$$$$$$$$$$$$$$$$$$      $$$$ $$$           $$$$    $$$$$$$$$$$$$$    $$$$$$$$$$$$$$$$$$$$$
+      $$$$            $$$$      $$$$  $$$          $$$$         $$$$         $$$$$
+      $$$$            $$$$      $$$$   $$$         $$$$         $$$$         $$$$$
+      $$$$            $$$$      $$$$    $$$        $$$$         $$$$         $$$$$
+      $$$$     $$     $$$$      $$$$     $$$       $$$$         $$$$         $$$$$$$$$$$$$$$$$$
+      $$$$$$$$$$$$$$$$$$$$      $$$$      $$$      $$$$         $$$$          $$$$$$$$$$$$$$$$$$
+      $$$$$$$$$$$$$$$$$$$$      $$$$       $$$     $$$$         $$$$           $$$$$$$$$$$$$$$$$$
+      $$$$            $$$$      $$$$        $$$    $$$$         $$$$                        $$$$$
+      $$$$            $$$$      $$$$         $$$   $$$$         $$$$                        $$$$$
+      $$$$            $$$$      $$$$          $$$  $$$$         $$$$                        $$$$$
+      $$$$            $$$$      $$$$           $$$ $$$$    $$$$$$$$$$$$$$   $$$$$$$$$$$$$$$$$$$$$
+     $$$$$$          $$$$$$    $$$$$$           $$$$$$$     $$$$$$$$$$$$     $$$$$$$$$$$$$$$$$$$
+    $$$$$$$$        $$$$$$$$  $$$$$$$$           $$$$$$      $$$$$$$$$$        $$$$$$$$$$$$$$$$
+
+                            LETS START OUR JOURNEY...................
+
+*/
+
+#include<bits/stdc++.h>
+#define speed ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define pb push_back
+#define pf push_front
+#define in insert
+#define ff first
+#define ss second
+#define off return 0
+#define mp make_pair
+#define MOD 1000000007
+#define ll long long int
+#define ld long double
+#define cllr system("COLOR F5")
+#define snll(a) scanf("%lld", &a)
+#define snll2(a,b) scanf("%lld %lld", &a,&b)
+#define snll3(a,b,c) scanf("%lld %lld %lld", &a,&b,&c)
+#define snl(b) scanf("%d", &b)
+#define snl2(a,b) scanf("%d %d", &a,&b)
+#define snl3(a,b,c) scanf("%d %d %d", &a,&b,&c)
+#define ptl(a) printf("%d\n", a)
+#define ptll(a) printf("%lld\n", a)
+#define ptl2(a,b) printf("%d %d\n", a,b)
+#define ptll2(a,b) printf("%lld %lld\n", a,b)
+#define gcd(a,b) __gcd(a,b)
+#define lcm(a,b) (a*b)/gcd(a,b)
+#define pi acos(-1)
+#define C continue
+#define B break
+#define N '\n'
+#define al(v) v.begin(),v.end()
+#define srt(v) sort(v.begin(),v.end())
+#define rev(v) reverse(v.begin(),v.end())
+#define loop(i,a,n) for(ll i=a;i<n;i++)
+using namespace std;
+int main()
+{
+    speed
+    int T;
+    cin>>T;
+    while(T--)
+    {
+      int n;
+      cin>>n;
+      int arr[n],c=0,d=0;
+      vector<int> va,vb;
+      for(int i=0;i<n;i++)
+      {
+          cin>>arr[i];
+          if(arr[i]&1) {
+                c++;
+                va.push_back(arr[i]);
+          }
+          else {
+                d++; vb.push_back(arr[i]);
+          }
+      }
+      if(c%2==0 && d%2==0)
+         cout << "YES" << N;
+      else
+      {
+          int f=0;
+          for(int i=0;i<va.size();i++)
+          {
+              for(int j=0;j<vb.size();j++)
+              {
+                  if(abs(va[i]-vb[j])==1)
+                  {
+                      f=1;B;
+                  }
+              }
+          }
+        if(f) cout << "YES" << N;
+        else cout << "NO" << N;
+      }
+    }
+    off;
+}
+//system("COLOR F5");
+/******************************************* THE END *************************************************/
+
